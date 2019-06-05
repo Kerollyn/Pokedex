84bac3e9e5d793eb1a3fc2196aaf801e0d3b34d0
Kerollyn <44269715+Kerollyn@users.noreply.github.com>
2019-06-04 22:26:10 -0300

    Add pasta Service

---
+++ Pokedex/Pokedex/Service/Interfaces/IPokemonService.cs
@@ -0,0 +1,16 @@
+﻿using Pokedex.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Pokedex.Service.Interfaces
+{
+    public interface IPokemonService
+    {
+        ObservableCollection<PokemonViewModel> Todos();
+        void Inserir(PokemonViewModel pokemon);
+        void Atualizar(PokemonViewModel pokemon);
+        void Excluir(PokemonViewModel pokemon);
+    }
+}
