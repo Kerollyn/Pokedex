e5588643cbd349b6ebb19c454a3fbc1c860afc65
Kerollyn <44269715+Kerollyn@users.noreply.github.com>
2019-06-04 22:43:25 -0300

    Add Criar Pokemon

---
+++ Pokedex/Pokedex/Service/Impl/InMemoryPokemonService.cs
@@ -0,0 +1,46 @@
+﻿using Pokedex.Service.Interfaces;
+using Pokedex.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Pokedex.Service.Impl
+{
+    public class InMemoryPokemonService : IPokemonService
+    {
+        private static ObservableCollection<PokemonViewModel> _pokemons = new ObservableCollection<PokemonViewModel>();
+
+        public InMemoryPokemonService()
+        {
+        }
+
+        public void Atualizar(PokemonViewModel pokemon)
+        {
+
+        }
+
+        public void Excluir(PokemonViewModel pokemon)
+        {
+            _pokemons.Remove(pokemon);
+        }
+
+        public void Inserir(PokemonViewModel pokemon)
+        {
+            _pokemons.Add(pokemon);
+        }
+
+        public ObservableCollection<PokemonViewModel> Todos()
+        {
+            if (_pokemons.Count == 0)
+            {
+                _pokemons.Add(new PokemonViewModel
+                {
+                    Numero = 1,
+                    Nome = "Bulbassauro"
+                });
+            }
+            return _pokemons;
+        }
+    }
+}
---
+++ Pokedex/Pokedex/ViewModels/CriarPokemonPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Pokedex.Service.Interfaces;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+
+namespace Pokedex.ViewModels
+{
+    public class CriarPokemonPageViewModel : BindableBase
+    {
+        private IPokemonService _pokemonService;
+        private INavigationService _navigationService;
+
+        private PokemonViewModel _pokemon = new PokemonViewModel();
+        public PokemonViewModel Pokemon
+        {
+            get { return _pokemon; }
+            set { SetProperty(ref _pokemon, value); }
+        }
+
+        public DelegateCommand SalvarCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    _pokemonService.Inserir(_pokemon);
+                    _navigationService.GoBackAsync();
+                });
+            }
+        }
+
+        public CriarPokemonPageViewModel(INavigationService navigationService, IPokemonService pokemonService)
+        {
+            _pokemonService = pokemonService;
+            _navigationService = navigationService;
+        }
+    }
+}
